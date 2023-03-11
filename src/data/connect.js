0492ae7769c01ed696c8d45ebeda9982944e1c37
Nikhil Kumar Jha <56074419+NikhilKumarJha@users.noreply.github.com>
2023-03-12 02:57:33 +0530

    .

---
+++ src/data/connect.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose');
+
+const {
+    DB_HOST,
+    DB_PORT,
+    DB_NAME
+}=process.env;
+
+const connectionStr=`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+mongoose.connect(connectionStr)
+    .then(()=>{
+        console.log(`connected to the ${DB_NAME}`); 
+    })
+    .catch(err=>{
+        console.log(err.message);
+        process.exit(1);
+    })
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('./data/connect');
 
 const express=require('express');
 const path=require('path');
