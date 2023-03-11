fafc10d5a1bf4784d66f452ef357f8712acab0a2
Nikhil Kumar Jha <56074419+NikhilKumarJha@users.noreply.github.com>
2023-03-12 01:30:43 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const path=require('path');
+
+const uiIndexRouter=require('./routes/ui/index');
+const uiAboutRouter=require('./routes/ui/about');
+const apiIndexRouter=require('./routes/api/v1/index');
+
+const app=express();
+
+app.set('views',path.join(process.cwd(),'views'));
+app.set('view engine','ejs');
+
+app.set('title','Platter');
+
+app.use(express.static(path.join(process.cwd(),'public')));
+app.use('/api/v1',apiIndexRouter);
+app.use(uiIndexRouter,uiAboutRouter);
+
+const PORT=process.env.PORT||3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+})
+.on('error',error=>console.log(error.message));
