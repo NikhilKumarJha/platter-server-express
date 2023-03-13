1ae22833e104894b0dc7fc1c009495d9451f9c13
Nikhil Kumar Jha <56074419+NikhilKumarJha@users.noreply.github.com>
2023-03-14 03:43:31 +0530

    .

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ require('./data/connect');
 const express=require('express');
 const path=require('path');
 
+const morgan=require('morgan');
+const logger=require('./middleware/logger');
+const winston=require('./middleware/winston.config');
+
 const apiIndexRouter=require('./routes/api/v1/index');
 const apiRestaurantRouter=require('./routes/api/v1/restaurants');
 const apiItemsRouter=require('./routes/api/v1/items');
 const uiIndexRouter=require('./routes/ui/index');
 const uiAboutRouter=require('./routes/ui/about');
 const uiRestaurantRouter=require('./routes/ui/restaurants');
-const logger=require('./middleware/logger');
 
 const app=express();
 
@@ -20,6 +23,8 @@ app.set('view engine','ejs');
 app.set('title','Platter');
 
 app.use(logger);
+app.use(morgan('combined',{stream:winston.stream}));
+
 app.use(express.static(path.join(process.cwd(),'public')));
 app.use('/api/v1',apiIndexRouter);
 app.use('/api/v1/restaurants',apiRestaurantRouter);
@@ -32,6 +37,7 @@ app.use('/restaurants',uiRestaurantRouter);
 const PORT=process.env.PORT||3000;
 
 app.listen(PORT,()=>{
-    console.log(`Server running on http://localhost:${PORT}`);
+    // console.log(`Server running on http://localhost:${PORT}`);
+    winston.info(`Server running on http://localhost:${PORT}`);
 })
-.on('error',error=>console.log(error.message));
+.on('error',error=>winston.error(error.message));
