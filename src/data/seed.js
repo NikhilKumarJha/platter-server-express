7b1c7d005aae3041108f3ea2d3232ddadb2d2c05
Nikhil Kumar Jha <56074419+NikhilKumarJha@users.noreply.github.com>
2023-03-14 00:46:28 +0530

    .

--- src/data/seed.js
+++ src/data/seed.js
@@ -9,6 +9,7 @@ try{
     console.log(process.cwd());
     console.log(__dirname);
     execSync(`mongoimport --db ${DB_NAME} --collection restaurants --drop --file "${path.join(process.cwd(),'src/data/seed/restaurants.json')}" --jsonArray`)
+    execSync(`mongoimport --db ${DB_NAME} --collection items --drop --file "${path.join(process.cwd(),'src/data/seed/items.json')}" --jsonArray`)
     console.log('successfully imported restaurants');
 }catch(err){
     console.log(err.message);
